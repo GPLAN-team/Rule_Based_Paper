8547623d0c6d7aeea1945d13816aa6e469cdf566
Yathaarth Jamwal <yathaarthjamwal@Yathaarths-MacBook-Air.local>
2022-12-21 02:54:02 +0530

    Removed unused files

--- Temp_Code/test2.py
+++
@@ -1,9 +0,0 @@
-from turtle import pos
-import networkx as nx
-import matplotlib.pyplot as plt
-import itertools
-
-arr = ["A","B","C","D","E","F"]
-perm = list(itertools.permutations(arr))
-print(perm)
-
